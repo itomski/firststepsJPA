3300465e62fcd0a0d98587fc95b33edc3877a7a7
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-02-12 13:29:01 +0100

    Update ORM

--- src/main/java/de/lubowiecki/jpa/firststeps/ProductApp.java
+++ src/main/java/de/lubowiecki/jpa/firststeps/ProductApp.java
@@ -1,19 +1,64 @@
 package de.lubowiecki.jpa.firststeps;
 
+import de.lubowiecki.jpa.firststeps.model.Product;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.sql.SQLException;
 
 public class ProductApp {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("FirstStepsPU");
-        EntityManager em = factory.createEntityManager();
+        try(EntityManagerFactory factory = Persistence.createEntityManagerFactory("FirstStepsPU");
+                EntityManager em = factory.createEntityManager()) {
+
+            // insert(em);
+            readOne(em);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // INSERT
+    private static void insert(EntityManager em) {
+        Product p1 = new Product();
+        p1.setName("Milch");
+        p1.setDescription("H Weidemilch. 3,5% Fett");
+        p1.setPrice(1.39);
+        p1.setAvailable(true);
+
+        em.getTransaction().begin(); // Start der Transaktion
+        em.persist(p1); // Nur Klassen mit der Annotation @Entity können von Hibernate verwaltet werden
+        em.getTransaction().commit(); // Aufgaben in der Transaktion werden an die DB übertragen
+    }
+
+    // SELECT
+    private static void readOne(EntityManager em) {
+
+        Product p = em.find(Product.class, 1); // Fragt alle Daten für den Datensatz mit der ID 1
+        System.out.println(p.getName());
+        System.out.println(p.getDescription());
+        System.out.println(p.getPrice());
+    }
+
+    private static void readAll(EntityManager em) {
+       //...
+    }
+
+    private static void delete(EntityManager em) {
+        //...
+    }
+
+    private static void readByGroup(EntityManager em) {
+        // Objekt-Beziehungen
+        // OneToOne
+        // OneToMany
+        // ManyToMany
 
         //...
-
-        em.close();
-        factory.close();
     }
 }
---
+++ src/main/java/de/lubowiecki/jpa/firststeps/model/Product.java
@@ -0,0 +1,69 @@
+package de.lubowiecki.jpa.firststeps.model;
+
+import jakarta.persistence.*;
+
+@Entity // Soll in der DB gespeichert werden
+@Table(name = "products") // Name der Tabelle. Standard ist gleich dem Klassennamen
+public class Product {
+
+    @Id // Ist der Primaty-Key
+    @GeneratedValue // Ist autoincrement
+    private int id;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(length = 1000)
+    private String description;
+
+    private double price;
+
+    private boolean available;
+
+    // private ProductGroup productGroup;
+
+    // private Set<Tag> tags;
+
+    // private Status status = Status.OK;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
